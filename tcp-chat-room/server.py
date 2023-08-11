7ac91ff764fe1b8ffd9fdf3d4a6b8a58c79e686a
Jon <jonathancirillo@outlook.com>
2023-08-11 12:29:55 -0400

    receive method

--- tcp-chat-room/server.py
+++ tcp-chat-room/server.py
@@ -21,7 +21,7 @@ def handle(client):
             message = client.recv(1024)
             broadcast(message)
         except Exception as e:
-            print(f'An error occured: {e}')
+            print(f'Exception: {e}')
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -29,3 +29,22 @@ def handle(client):
             broadcast(f'Nickname: {nickname} was removed'.encode('ascii'))
             nicknames.remove(nickname)
             break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+        
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of client is: {nickname}')
+        broadcast(f'{nickname} has joined'.encode('ascii'))
+        client.send('Connected to the server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=client)
+        thread.start()
+
+receive()
