581f75b81cd2b04c9c982474e20d03f780f38d64
Jon <jonathancirillo@outlook.com>
2023-08-11 10:34:38 -0400

    setup message // add image// added error handling

--- mailing-client/main.py
+++ mailing-client/main.py
@@ -1,15 +1,34 @@
 import smtplib, os
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from dotenv import load_dotenv
 load_dotenv()
 EMAIL_PASS = os.getenv('EMAIL_PASS')
 EMAIL = os.getenv('EMAIL')
 
-print(EMAIL_PASS, EMAIL)
+try:
+    # Connect to the email server
+    server = smtplib.SMTP('smtp-mail.outlook.com', 587)
+    server.starttls()
+    print("Successfully connected to the server.")
+except:
+    print("Error connecting to the server.")
 
-# Connect to the email server
-server = smtplib.SMTP('smtp-mail.outlook.com', 587)
-server.starttls()
+try:
+    # Login to your email
+    server.login(EMAIL, EMAIL_PASS)
+    print("Successfully logged in.")
+except:
+    print("Error logging in.")
 
-# Login to your email
-server.login(EMAIL, EMAIL_PASS)
+msg = MIMEMultipart()
+msg['From'] = 'Jon'
+msg['To'] = 'jonathan.cirillo@ucf.edu'
+msg['Subject'] = 'Just a Test'
+
+with open('message.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
