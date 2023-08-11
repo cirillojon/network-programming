ad689bf48e50e69043886cdfca9fed7eb467748b
Jon <jonathancirillo@outlook.com>
2023-08-11 12:39:03 -0400

    define receive and write methods for client

--- tcp-chat-room/client.py
+++ tcp-chat-room/client.py
@@ -1,5 +1,25 @@
 import threading
 import socket
 
+nickname = input("Choose a nickname: ")
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(f'Message: {message}')
+        except Exception as e:
+            print(f'An error occurred: {e}')
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
